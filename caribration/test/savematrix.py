b226438d0b4ab558e6960e5fdef25a42f7858779
Peeradon Ruengkaew <76780514+peeradonmoke2002@users.noreply.github.com>
2025-04-09 11:39:07 +0700

    update calibration matrix config

---
+++ caribration/test/savematrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+import json
+
+best_matrix = np.array([[ 0.00465481, -0.99626089,  0.01972409, -0.08235386],
+                        [ 0.1348973,   0.009654,    1.02464258, -0.06076352],
+                        [-0.98427042,  0.00608958,  0.08377368, -0.10207071],
+                        [ 0.0,         0.0,         0.0,         1.0       ]])
+
+data = {
+    "name": "camera_to_world_transform",
+    "matrix": best_matrix.tolist()
+}
+
+with open(r'FRA631_Project_Dual_arm_UR5_Calibration\caribration\config\best_matrix.json', 'w') as f:
+    json.dump(data, f, indent=4)
