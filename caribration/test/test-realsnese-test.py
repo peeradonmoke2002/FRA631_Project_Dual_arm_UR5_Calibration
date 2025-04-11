f016b907c671d5955f7482c9cd8e47f74d7f7fb5
Peeradon Ruengkaew <76780514+peeradonmoke2002@users.noreply.github.com>
2025-04-11 21:21:12 +0700

    fix convert point3d

--- caribration/test/test-realsnese-test.py
+++ caribration/test/test-realsnese-test.py
@@ -13,8 +13,8 @@ robot = robot_movement.RobotControl()
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_1000)
 image_marked, point3d = cam.get_board_pose(aruco_dict)
 print("3D Points:", point3d)
-point3d_world = robot.convert_cam_to_world(point3d.to_list())
-print("3D Points in World Reference:", point3d_world)
+# point3d_world = robot.convert_cam_to_world(point3d.to_list())
+# print("3D Points in World Reference:", point3d_world)
 
 
 # Show image if available
