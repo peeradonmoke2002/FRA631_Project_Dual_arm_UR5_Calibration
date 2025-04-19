eb9ba0cbab0c9126a259b7bfc27ff6e5a0edfc1f
Peeradon Ruengkaew <76780514+peeradonmoke2002@users.noreply.github.com>
2025-04-19 17:41:56 +0700

    Update caribration-affine_matrix.py

--- caribration/caribration-affine_matrix.py
+++ caribration/caribration-affine_matrix.py
@@ -50,7 +50,7 @@ calibrator = CalibratorTools(calibration_data_list)
 
 # Set calibration parameters.
 num_selected_positions = len(calibration_data_list)  # using all loaded data points
-num_iterations = 200000
+num_iterations = 20000
 target_rms_error = 0.001
 # Run the calibration search.
 best_matrix, best_rms, rms_errors, selected_positions, transformed_points = calibrator.find_best_matrix(
